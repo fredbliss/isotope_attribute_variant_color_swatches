39c1aab31d63cd25c883d09baf7cfc500bba60eb
fbliss <fred.bliss@intelligentspark.com>
2017-11-01 19:37:54 -0400

    added dca for attribute editing

---
+++ dca/tl_iso_attribute.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * listings attribute for Isotope eCommerce
+ *
+ * @copyright jedoStyle 2016 <http://www.jedo-style.de>
+ * @author Jens Doberenz <github@jedo-style.de>
+ * @licence LGPL
+ */
+
+
+
+// Palette
+
+$GLOBALS['TL_DCA']['tl_iso_attribute']['palettes']['VariantColorSwatches'] = $GLOBALS['TL_DCA']['tl_iso_attribute']['palettes']['selectvariant_option'];
